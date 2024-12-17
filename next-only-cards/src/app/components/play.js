e1f7f3a085f366c5913b2d0a9f26d27fae21ecc0
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-12-17 12:50:32 +0530

    Update play.js

--- next-only-cards/src/app/components/play.js
+++ next-only-cards/src/app/components/play.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function PlayButton() {
   return (
     <Link 
-      href="/play"
+      href="https://github.com/DAOminators/OC"
       className="fixed bottom-8 right-8 z-50 bg-ablue hover:bg-blue-600 text-white font-bold py-4 px-8 rounded-full transform hover:scale-110 transition-all duration-300 shadow-lg text-xl"
     >
       Play Now
